fb9c0babaf9485f0bf994e0b4acfdd341cc1228d
peaky76 <robertjamespeacock@gmail.com>
2023-10-16 18:19:28 +0100

    refactor: split book/news/web into different fns

--- wikicite/wikicite.py
+++ wikicite/wikicite.py
@@ -12,8 +12,59 @@ with open(os.path.join(__location__, "./sources.toml"), "rb") as file:
     sources = tomllib.load(file)
 
 
-@click.command()
-@click.argument("cite_type", type=click.Choice(CITE_TYPES, case_sensitive=False))
+@click.group()
+def cite():
+    pass
+
+
+@cite.command()
+@click.option(
+    "-t", "--title", type=str, prompt=True, default="", help="title of article"
+)
+@click.option(
+    "-a",
+    "--author",
+    type=str,
+    nargs=2,
+    prompt=True,
+    default=["", ""],
+    help="author first and last name (use quotes around multi-part names)",
+)
+@click.option("-p", "--publisher", type=str, prompt=True, help="publisher")
+@click.option("-l", "--location", type=str, prompt=True, help="location of publisher")
+@click.option("-d", "--date", type=str, prompt=True, help="year of publication")
+@click.option("-isbn", type=str, prompt=True, default="", help="isbn of book")
+def book(title, author, publisher, location, date, isbn):
+    ref_name = (
+        f"{author[1]}{date}"
+        if author[1]
+        else f"{title[:6].replace(' ','').title()}{date}"
+    )
+
+    attributes = {
+        "last": author[1],
+        "first": author[0],
+        "title": title,
+        "location": location,
+        "publisher": publisher,
+        "isbn": isbn,
+        "date": date.strip(),
+    }
+    attributes = {key: val for key, val in attributes.items() if val}
+
+    click.echo(f"<ref name={ref_name}>")
+    click.echo(
+        md.template(
+            " |".join(
+                [f"cite book"]
+                + [f"{key}={val}" for key, val in attributes.items() if val]
+            )
+        )
+    )
+    click.echo("</ref>")
+
+
+@cite.command()
 @click.option(
     "-s",
     "--source",
@@ -33,14 +84,11 @@ with open(os.path.join(__location__, "./sources.toml"), "rb") as file:
 @click.option(
     "-t", "--title", type=str, prompt=True, default="", help="title of article"
 )
-@click.option("-p", "--publisher", type=str, prompt=True, help="publisher")
-@click.option("-l", "--location", type=str, prompt=True, help="location of publisher")
 @click.option("-d", "--date", type=str, prompt=True, help="date of publication")
 @click.option(
     "-url", type=str, prompt=True, default="", help="url where the source can be found"
 )
-@click.option("-isbn", type=str, prompt=True, default="", help="isbn of book")
-def cite(cite_type, source, author, title, publisher, location, date, url, isbn):
+def news(source, author, title, date, url):
     source = sources[source] if source else None
 
     raw_date = datetime.strptime(str(date), "%d%m%Y") or None
@@ -51,7 +99,7 @@ def cite(cite_type, source, author, title, publisher, location, date, url, isbn)
         if author[1]
         else f"{source['abbr']}{ymd_date}"
         if source
-        else f"{title[:6]}{ymd_date}"
+        else f"{title[:6].replace(' ','').title()}{ymd_date}"
     )
 
     attributes = {
@@ -59,10 +107,8 @@ def cite(cite_type, source, author, title, publisher, location, date, url, isbn)
         "first": author[0],
         "title": title,
         "work": md.link(source["name"]) if source else "",
-        "location": source["location"] if source else location,
-        "publisher": publisher,
+        "location": source["location"] if source else "",
         "url": url,
-        "isbn": isbn,
         "date": article_date.strip(),
         "access-date": TODAY,
     }
@@ -72,7 +118,197 @@ def cite(cite_type, source, author, title, publisher, location, date, url, isbn)
     click.echo(
         md.template(
             " |".join(
-                [f"cite {cite_type}"]
+                [f"cite news"]
+                + [f"{key}={val}" for key, val in attributes.items() if val]
+            )
+        )
+    )
+    click.echo("</ref>")
+
+
+from datetime import date, datetime
+import os
+import click
+import tomllib
+import wikicite.markdown as md
+
+
+TODAY = date.today().strftime("%d %B %Y")
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+with open(os.path.join(__location__, "./sources.toml"), "rb") as file:
+    sources = tomllib.load(file)
+
+
+@click.group()
+def cite():
+    pass
+
+
+@cite.command()
+@click.option(
+    "-t", "--title", type=str, prompt=True, default="", help="title of article"
+)
+@click.option(
+    "-a",
+    "--author",
+    type=str,
+    nargs=2,
+    prompt=True,
+    default=["", ""],
+    help="author first and last name (use quotes around multi-part names)",
+)
+@click.option("-p", "--publisher", type=str, prompt=True, help="publisher")
+@click.option("-l", "--location", type=str, prompt=True, help="location of publisher")
+@click.option("-d", "--date", type=str, prompt=True, help="year of publication")
+@click.option("-isbn", type=str, prompt=True, default="", help="isbn of book")
+def book(title, author, publisher, location, date, isbn):
+    ref_name = (
+        f"{author[1]}{date}"
+        if author[1]
+        else f"{title[:6].replace(' ','').title()}{date}"
+    )
+
+    attributes = {
+        "last": author[1],
+        "first": author[0],
+        "title": title,
+        "location": location,
+        "publisher": publisher,
+        "isbn": isbn,
+        "date": date.strip(),
+    }
+    attributes = {key: val for key, val in attributes.items() if val}
+
+    click.echo(f"<ref name={ref_name}>")
+    click.echo(
+        md.template(
+            " |".join(
+                [f"cite book"]
+                + [f"{key}={val}" for key, val in attributes.items() if val]
+            )
+        )
+    )
+    click.echo("</ref>")
+
+
+@cite.command()
+@click.option(
+    "-s",
+    "--source",
+    type=click.Choice(sources.keys()),
+    default=None,
+    help="use one of the pre-configured common sources",
+)
+@click.option(
+    "-a",
+    "--author",
+    type=str,
+    nargs=2,
+    prompt=True,
+    default=["", ""],
+    help="author first and last name (use quotes around multi-part names)",
+)
+@click.option(
+    "-t", "--title", type=str, prompt=True, default="", help="title of article"
+)
+@click.option("-d", "--date", type=str, prompt=True, help="date of publication")
+@click.option(
+    "-url", type=str, prompt=True, default="", help="url where the source can be found"
+)
+def news(source, author, title, date, url):
+    source = sources[source] if source else None
+
+    raw_date = datetime.strptime(str(date), "%d%m%Y") or None
+    article_date = raw_date.strftime("%e %B %Y") or ""
+    ymd_date = raw_date.strftime("%y%m%d") or ""
+    ref_name = (
+        f"{author[1]}{ymd_date}"
+        if author[1]
+        else f"{source['abbr']}{ymd_date}"
+        if source
+        else f"{title[:6].replace(' ','').title()}{ymd_date}"
+    )
+
+    attributes = {
+        "last": author[1],
+        "first": author[0],
+        "title": title,
+        "work": md.link(source["name"]) if source else "",
+        "location": source["location"] if source else "",
+        "url": url,
+        "date": article_date.strip(),
+        "access-date": TODAY,
+    }
+    attributes = {key: val for key, val in attributes.items() if val}
+
+    click.echo(f"<ref name={ref_name}>")
+    click.echo(
+        md.template(
+            " |".join(
+                [f"cite news"]
+                + [f"{key}={val}" for key, val in attributes.items() if val]
+            )
+        )
+    )
+    click.echo("</ref>")
+
+
+@cite.command()
+@click.option(
+    "-s",
+    "--source",
+    type=click.Choice(sources.keys()),
+    default=None,
+    help="use one of the pre-configured common sources",
+)
+@click.option(
+    "-a",
+    "--author",
+    type=str,
+    nargs=2,
+    prompt=True,
+    default=["", ""],
+    help="author first and last name (use quotes around multi-part names)",
+)
+@click.option(
+    "-t", "--title", type=str, prompt=True, default="", help="title of article"
+)
+@click.option("-d", "--date", type=str, prompt=True, help="date of publication")
+@click.option(
+    "-url", type=str, prompt=True, default="", help="url where the source can be found"
+)
+def web(source, author, title, date, url):
+    source = sources[source] if source else None
+
+    raw_date = datetime.strptime(str(date), "%d%m%Y") or None
+    article_date = raw_date.strftime("%e %B %Y") or ""
+    ymd_date = raw_date.strftime("%y%m%d") or ""
+    ref_name = (
+        f"{author[1]}{ymd_date}"
+        if author[1]
+        else f"{source['abbr']}{ymd_date}"
+        if source
+        else f"{title[:6].replace(' ','').title()}{ymd_date}"
+    )
+
+    attributes = {
+        "last": author[1],
+        "first": author[0],
+        "title": title,
+        "work": md.link(source["name"]) if source else "",
+        "location": source["location"] if source else "",
+        "url": url,
+        "date": article_date.strip(),
+        "access-date": TODAY,
+    }
+    attributes = {key: val for key, val in attributes.items() if val}
+
+    click.echo(f"<ref name={ref_name}>")
+    click.echo(
+        md.template(
+            " |".join(
+                [f"cite web"]
                 + [f"{key}={val}" for key, val in attributes.items() if val]
             )
         )
